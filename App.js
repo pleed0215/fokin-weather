528793703d8cc65d514f839f1d802ab1e8a8aaea
이은덕 <pleed0215@ieundeog-ui-MacBookPro.local>
2020-06-10 06:49:58 +0900

    meaningless commit

--- App.js
+++ App.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Loading from "./Loading";
+import * as Location from "expo-location";
+import { View, Text } from "react-native";
+
 // 웬만하면 flex에서 레이아웃 작업 권장.
 export default function App() {
-  return <Loading />;
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  });
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+  return (
+    <View style={{ paddingTop: 50 }}>
+      <Text>{text}</Text>
+    </View>
+  );
 }
