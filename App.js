4b34d46db8434f3f10d36fda37df1ff66b2e8ce8
이은덕 <pleed0215@ieundeog-ui-MacBookPro.local>
2020-06-09 15:49:29 +0900

    haha

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.fucking}>뻑킹 혜진</Text>
+      <Text style={styles.fucking}>뻑킹 혜진</Text>
     </View>
   );
 }
@@ -12,8 +13,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  fucking: {
+    color: "white",
+    fontWeight: "900",
   },
 });
