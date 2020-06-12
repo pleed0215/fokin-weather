0d1096400b07bf9f6567493f700f44ce44927a07
이은덕 <pleed0215@ieundeog-ui-MacBookPro.local>
2020-06-12 21:22:12 +0900

    getting weather api... ended

--- App.js
+++ App.js
@@ -1,22 +1,46 @@
 import React, { useState, useEffect } from "react";
 import Loading from "./Loading";
 import * as Location from "expo-location";
-import { View, Text } from "react-native";
+import axios from "axios";
+import { View, Text, Alert } from "react-native";
+import Weather from "./Weather";
+
+const OWA_KEY = "2e46d3899d3ca08b0137e22b2e4857b9";
+const WEATHER_API = "https://api.openweathermap.org/data/2.5/weather";
 
 // 웬만하면 flex에서 레이아웃 작업 권장.
 export default function App() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
     (async () => {
-      let { status } = await Location.requestPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
-      }
+      try {
+        // trying to get location permission.
+        let { status } = await Location.requestPermissionsAsync();
+        if (status !== "granted") {
+          setErrorMsg("Permission to access location was denied");
+        }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
+        // getting current position.
+        let location = await Location.getCurrentPositionAsync({});
+        let { data: currWeather } = await axios.get(WEATHER_API, {
+          params: {
+            lat: location.coords.latitude,
+            lon: location.coords.longitude,
+            appid: OWA_KEY,
+            units: "metric",
+          },
+        });
+
+        setLocation(location);
+        setWeather(currWeather);
+        setLoading(false);
+      } catch (err) {
+        Alert.alert("Error in initializing", "haha. you're poor");
+      }
     })();
   });
   let text = "Waiting..";
@@ -25,9 +49,6 @@ export default function App() {
   } else if (location) {
     text = JSON.stringify(location);
   }
-  return (
-    <View style={{ paddingTop: 50 }}>
-      <Text>{text}</Text>
-    </View>
-  );
+  console.log(weather);
+  return isLoading ? <Loading /> : <Weather temp={weather.main.temp} />;
 }
